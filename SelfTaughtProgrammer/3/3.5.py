94c6ccd297ef707d55a229292a711efc28ba90bb
Павел Никуленок <pashaaaa86@gmail.com>
2023-06-28 19:07:33 +0500

    Подправил решение задачи 3.5

--- SelfTaughtProgrammer/3/3.5.py
+++ SelfTaughtProgrammer/3/3.5.py
@@ -20,22 +20,26 @@ def main() -> None:
     """
 
     try:
-        a: int = int(input("Введите делимое число: "))
-        b: int = int(input("Введите делитель: "))
+        a_input: str = input("Введите делимое число: ")
+        b_input: str = input("Введите делитель: ")
+
+        if not a_input.isdigit() or not b_input.isdigit():
+            raise ValueError("Введено некорректное значение. Ожидаются целые числа.")
+
+        a: int = int(a_input)
+        b: int = int(b_input)
 
         if a == 0:
             raise ZeroDivisionError("Делимое не может быть нулем.")
         elif b == 0:
             raise ZeroDivisionError("Делитель не может быть нулем.")
-        elif not isinstance(a, int) or not isinstance(b, int):
-            raise ValueError("Введено некорректное значение. Ожидаются целые числа.")
         else:
             print(divide(a, b))
 
-    except ValueError:
-        print("Некорректный ввод. Введите целое число.")
+    except ValueError as v_error:
+        print(f"Ошибка ввода: {v_error}")
     except ZeroDivisionError as error:
-        print(error)
+        print(f"Ошибка ввода: {error}")
 
 
 if __name__ == '__main__':
