fecf053e3efb75ea41193e8caddb4c36d38d34e5
Павел Никуленок <pashaaaa86@gmail.com>
2023-06-29 21:52:49 +0500

    Подправил решение задачи 3.1

--- SelfTaughtProgrammer/3/3.1.py
+++ SelfTaughtProgrammer/3/3.1.py
@@ -5,13 +5,34 @@ def main() -> None:
     Вызов главной функции.
     :return: None
     """
-    a: str = input("Введите строку - 1: ")
-    b: str = input("Введите строку - 2: ")
-    c: str = input("Введите строку - 3: ")
 
-    print(a)
-    print(b)
-    print(c)
+    fields = [
+        ("Введите строку 1: ", "str_one"),
+        ("Введите строку 2: ", "str_two"),
+        ("Введите строку 3: ", "str_three")
+    ]
+
+    data = {}
+    is_valid = False
+
+    while not is_valid:
+        for prompt, var_name in fields:
+            while True:
+                value = input(prompt)
+                if not value:
+                    print("Поле не может быть пустым")
+                elif value.isdigit():
+                    print("Введена только цифра")
+                else:
+                    data[var_name] = value
+                    break
+
+        is_valid = True
+
+    print("Все данные введены корректно.")
+    print("Результаты ввода:")
+    for var_name, value in data.items():
+        print(f"{var_name}: {value}")
 
 
 if __name__ == '__main__':
