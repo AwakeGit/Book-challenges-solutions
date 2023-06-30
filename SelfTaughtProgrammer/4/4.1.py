8f1b22399ae5e3e92f3461fc1b7e9c3c292aa32e
Павел Никуленок <pashaaaa86@gmail.com>
2023-06-30 17:08:16 +0500

    Исправил решение задачи 4.1

--- SelfTaughtProgrammer/4/4.1.py
+++ SelfTaughtProgrammer/4/4.1.py
@@ -1,13 +1,17 @@
 # 4.1	Напишите функцию, которая принимает число в качестве ввода, возводит его в квадрат и возвращает.
 
-def calculator(a: int) -> int:
+def calculator(a: str) -> float:
     """
     Возвращает квадрат числа
     :param a: Целое число
     :return: int
     """
+    a = float(a)
+
     if a == 0:
-        raise ValueError("У числа 0 нету квадрата")
+        raise ValueError("У числа 0 нет квадрата.")
+    elif not a:
+        raise ValueError("Только цифры.")
     return a * a
 
 
@@ -17,17 +21,14 @@ def main() -> None:
     :return: None
     """
 
-    try:
-        number_str = input("Введите число: ")
-        if not number_str.isdigit():
-            raise ValueError("Введено некорректное значение. Ожидаются целое число.")
+    while True:
+        try:
+            a_input: str = input("Введите целое число: ")
+            print(calculator(a_input))
+            break
 
-        number = int(number_str)
-        result = calculator(number)
-        print(f"{number} в квадрате = {result}")
-
-    except ValueError as e:
-        print(f"Ошибка ввода: {e}")
+        except ValueError as e:
+            print(f"Ошибка ввода: {e}")
 
 
 if __name__ == '__main__':
